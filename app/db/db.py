6eb6bd595ea3bf22e2665ab59e7be28deaf45b2d
mrodriguez-dev <marodriguezre@outlook.com>
2026-01-15 16:01:06 -0500

    se crea conexion exitosa con el database

--- app/db/db.py
+++ app/db/db.py
@@ -1,1 +1,20 @@
-print("hi database")
+
+
+import mysql.connector
+
+def conectar():
+    conexion = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="Root1234!",
+        database="peluqueria"
+    )
+
+    if conexion.is_connected():
+        print("✅ Conexión exitosa a la base de datos")
+    else:
+        print("❌ Falló la conexión")
+
+    return conexion
+
+conectar()
